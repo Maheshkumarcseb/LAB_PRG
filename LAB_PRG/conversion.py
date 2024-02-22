c9bbd640609a1d68c205f857c5ce548e300915cb
Mahesh kumar <mahesh.mahi24102004@gmail.com>
2024-02-22 17:44:58 +0530

    first commit

---
+++ LAB_PRG/conversion.py
@@ -0,0 +1,14 @@
+def bin2Dec(val): 
+    return int(val, 2) 
+def oct2Hex(val): 
+    return int(val, 8) 
+try: 
+    num1 = input("Enter a binary number : ") 
+    print(bin2Dec(num1)) 
+except ValueError: 
+    print("Invalid literal in input with base 2") 
+try: 
+    num2 = input("Enter a octal number : ") 
+    print(oct2Hex(num2)) 
+except ValueError: 
+    print("Invalid literal in input with base 8")
